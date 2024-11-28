8e0c3da28272ced3f728781152d43646a65eb8d2
Tivanov99 <trifon.ivanov21@gmail.com>
2024-11-28 22:17:48 +0200

    fix services

--- MobileWorld/MobileWorld/src/app/services/car-ads-service.ts
+++ MobileWorld/MobileWorld/src/app/services/car-ads-service.ts
@@ -3,12 +3,12 @@ import { Injectable } from "@angular/core";
 
 @Injectable({providedIn: 'root'})
 export class CarAdsService {
-  
+
   constructor(private http: HttpClient) {
   }
 
   getCarAds() : void{
     this.http.get('https://mobileworld-18285-default-rtdb.firebaseio.com/car_ads.json')
-    .subscribe((res) => {console.log(res)});
-}
+      .subscribe((res) => {console.log(res)});
+  }
 }
