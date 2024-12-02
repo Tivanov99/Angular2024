2eb8cc7b49a1a6a212230245d8516c806d18bf26
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-02 21:10:07 +0200

    add home page and disable AppComponent

--- MobileWorld/MobileWorld/src/app/base-page/base-page.component.ts
+++ MobileWorld/MobileWorld/src/app/base-page/base-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-base-page',
-  imports: [],
+  imports: [ NavBarComponent ],
   templateUrl: './base-page.component.html',
-  styleUrl: './base-page.component.css'
+  styleUrl: './base-page.component.css',
+  standalone : true
 })
 export class BasePageComponent {
 
