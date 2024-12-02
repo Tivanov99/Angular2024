70de164562a98f7bc281de2950725a0987cbaf04
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-02 21:52:02 +0200

    Encapsulate bootstrap to angular config

--- MobileWorld/MobileWorld/src/app/home-page/home-page.component.ts
+++ MobileWorld/MobileWorld/src/app/home-page/home-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { BasePageComponent } from '../base-page/base-page.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ BasePageComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  standalone : true
 })
-export class HomePageComponent {
+export class HomePageComponent extends BasePageComponent {
 
+  constructor() {
+    super();
+    
+  }
+  
 }
--- MobileWorld/MobileWorld/src/main.ts
+++ MobileWorld/MobileWorld/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { HomePageComponent } from './app/home-page/home-page.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(HomePageComponent, appConfig)
   .catch((err) => console.error(err));
