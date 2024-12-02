2e79a60287e3d3d742e309b3e9400543ea217328
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-02 23:39:26 +0200

    adding router to navigation component

--- MobileWorld/MobileWorld/src/app/nav-bar/nav-bar.component.ts
+++ MobileWorld/MobileWorld/src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { SessionManagerService } from '../services/session-manager-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [SessionManagerService, Router],
+  standalone: true,
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
 
-  constructor(public _sessionManagerService : SessionManagerService) {
+  constructor(public _sessionManagerService : SessionManagerService, private router: Router) {
     
   }
+  
 }
