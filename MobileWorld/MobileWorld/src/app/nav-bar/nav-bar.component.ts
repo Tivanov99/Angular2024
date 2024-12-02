9aa46d970b54f0c51ed2a69d38f80e5dd96bf422
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-02 23:58:16 +0200

    .

--- MobileWorld/MobileWorld/src/app/nav-bar/nav-bar.component.ts
+++ MobileWorld/MobileWorld/src/app/nav-bar/nav-bar.component.ts
@@ -1,18 +1,49 @@
 import { Component } from '@angular/core';
 import { SessionManagerService } from '../services/session-manager-service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [SessionManagerService, Router],
+  imports: [],
   standalone: true,
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
 
-  constructor(public _sessionManagerService : SessionManagerService, private router: Router) {
+  constructor(public _sessionManagerService : SessionManagerService) {
     
   }
-  
+
+  onRegistrationClick() : void{
+
+  }
+
+  onLoginClick() : void{
+
+  }
+
+  onLogoutClick() : void{
+    
+  }
+
+  onHomeClick(): void {
+
+  }
+
+  onAllAddsClick(): void {
+    console.log('onAllAddsClick');
+  }
+
+  onFavouritesClick() : void{
+
+  }
+
+  onAddVIPAddClick() : void{
+
+  }
+
+  onForumClick() : void{
+
+  }
+
 }
