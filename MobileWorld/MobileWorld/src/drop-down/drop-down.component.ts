4ce4253b15c2fa51060dc1db7786ee2a9f6e189b
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-03 23:26:01 +0200

    Added drop down component and selection

---
+++ MobileWorld/MobileWorld/src/drop-down/drop-down.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { DropDownModel } from '../app/models/drop-down-model';
+
+@Component({
+  selector: 'app-drop-down',
+  imports: [],
+  templateUrl: './drop-down.component.html',
+  styleUrl: './drop-down.component.css',
+  standalone: true
+})
+export class DropDownComponent {
+
+  @Input() dropDownTitle: string = "";
+  @Input() dropDownItems : DropDownModel[] = [];
+  private _selectedItemID : string = "";
+
+  constructor() {
+    
+  }
+
+  onSelectedItem(event: Event) : void{
+
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const selectedId = selectedOption.id;
+
+    this._selectedItemID = selectedId;
+    
+    console.log(selectedId);
+  }
+
+}
