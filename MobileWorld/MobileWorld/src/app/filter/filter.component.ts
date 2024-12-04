39083f8b4e9a90b9ae08d009f25e52d9f408b714
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-04 19:55:06 +0200

    added a component for the filter that will contain the entire construction

---
+++ MobileWorld/MobileWorld/src/app/filter/filter.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { DropDownComponent } from '../../drop-down/drop-down.component';
+import { DropDownModel } from '../models/drop-down-model';
+
+@Component({
+  selector: 'app-filter',
+  imports: [ DropDownComponent],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css',
+  standalone : true
+})
+export class FilterComponent {
+
+  public carBrandItems : DropDownModel[] = [];
+  public carBrandModelsItems : DropDownModel[] = [];
+  
+  constructor() {
+    
+    this.loadCarBrand();
+    this.loadCarModels();
+  }
+
+  loadCarBrand(){
+
+    this.carBrandItems.push({
+      name : "BMW",
+      itemID : 1
+    },
+    {
+      name : "Аudi",
+      itemID : 2
+    })
+
+  }
+
+  loadCarModels(){
+    this.carBrandModelsItems.push({
+      name : "A7",
+      itemID : 3
+    },
+    {
+      name : "RS7",
+      itemID : 4
+    })
+  }
+
+  loadFuelTypes(){
+    
+  }
+
+  loadTransmissionTypes(){
+    
+  }
+
+}
--- MobileWorld/MobileWorld/src/app/home-page/home-page.component.ts
+++ MobileWorld/MobileWorld/src/app/home-page/home-page.component.ts
@@ -2,57 +2,20 @@ import { Component } from '@angular/core';
 import { BasePageComponent } from '../base-page/base-page.component';
 import { DropDownComponent } from '../../drop-down/drop-down.component';
 import { DropDownModel } from '../models/drop-down-model';
+import { FilterComponent } from '../filter/filter.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ BasePageComponent, DropDownComponent],
+  imports: [ BasePageComponent, DropDownComponent, FilterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
   standalone : true
 })
 export class HomePageComponent  extends BasePageComponent{
 
-  public carBrandItems : DropDownModel[] = [];
-  public carBrandModelsItems : DropDownModel[] = [];
-  
   constructor() {
     super();
 
-    this.loadCarBrand();
-    this.loadCarModels();
   }
-
-  loadCarBrand(){
-
-    this.carBrandItems.push({
-      name : "BMW",
-      itemID : 1
-    },
-    {
-      name : "Аudi",
-      itemID : 2
-    })
-
-  }
-
-  loadCarModels(){
-    this.carBrandModelsItems.push({
-      name : "A7",
-      itemID : 3
-    },
-    {
-      name : "RS7",
-      itemID : 4
-    })
-  }
-
-  loadFuelTypes(){
-    
-  }
-
-  loadTransmissionTypes(){
-    
-  }
-
 
 }
