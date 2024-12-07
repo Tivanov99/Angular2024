9dea824552bbc83b87e1661dd1e25607a95692f7
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-07 13:52:02 +0200

    added ad details component

---
+++ MobileWorld/MobileWorld/src/app/add-details/add-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-details',
+  imports: [],
+  templateUrl: './add-details.component.html',
+  styleUrl: './add-details.component.css',
+  standalone : true
+})
+export class AddDetailsComponent {
+
+}
