29af78436cfcde351c00323e999b3a1bd08dd71a
Tivanov99 <trifon.ivanov21@gmail.com>
2024-12-07 13:49:55 +0200

    added forum component, and routing to that component

--- MobileWorld/MobileWorld/src/app/app.routes.ts
+++ MobileWorld/MobileWorld/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AdsComponent } from './ads/ads.component';
+import { ForumComponent } from './forum/forum.component';
 
 export const routes: Routes = [
     {path : '', redirectTo: '/home', pathMatch: 'full'}, 
@@ -10,4 +11,5 @@ export const routes: Routes = [
     {path: 'registration', component : RegistrationComponent},
     {path: 'login', component : LoginComponent},
     {path: 'ads', component : AdsComponent},
+    {path: 'forum', component : ForumComponent},
 ];
---
+++ MobileWorld/MobileWorld/src/app/forum/forum.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forum',
+  imports: [],
+  templateUrl: './forum.component.html',
+  styleUrl: './forum.component.css'
+})
+export class ForumComponent {
+
+}
